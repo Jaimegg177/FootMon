3df979a8f0e54b53d9ef094ca268d66482eb044a
antoniopelaezmoreno <antpelmor@alum.us.es>
2024-04-22 18:02:04 +0200

    popular con csv

--- footmon.py
+++ footmon.py
@@ -5,16 +5,34 @@ Created on 11 oct 2023
 '''
 
 from tkinter import Tk, Button
-
+import pandas as pd
 from pymongo import MongoClient
 
 def bd():
     # Conexión a MongoDB
     client = MongoClient('mongodb://localhost:27017/')
     db = client['deportes_db']
-    partidos = db['partidos']
+    results = db['results']
+    goalscorers = db['goalscorers']
+    shootouts = db['shootouts']
+
+    # Insertar resultados
+    data_results = pd.read_csv('data/results.csv')
+    data_goalscorers = pd.read_csv('data/goalscorers.csv')
+    data_shootouts = pd.read_csv('data/shootouts.csv')
+
+    # Convierte los datos a un formato compatible con MongoDB
+    data_results_json = data_results.to_dict(orient='records')
+    data_goalscorers_json = data_goalscorers.to_dict(orient='records')
+    data_shootouts_json = data_shootouts.to_dict(orient='records')
+
+    # Inserta los datos en la colección de MongoDB
+    results.insert_many(data_results_json)
+    goalscorers.insert_many(data_goalscorers_json)
+    shootouts.insert_many(data_shootouts_json)
 
     # Función de map
+    '''
     map_function = """
     function() {
         var resultado;
@@ -121,7 +139,7 @@ def bd():
         print(f"Tarjetas rojas: Local {doc['value']['tarjetas_rojas_local']}, Visitante: {doc['value']['tarjetas_rojas_visitante']}")
         print(f"Corners: Local {doc['value']['corners_local']}, Visitante: {doc['value']['corners_visitante']}")
         print(f"Minutos jugados: Local {doc['value']['minutos_local']}, Visitante: {doc['value']['minutos_visitante']}")
-
+    '''
 
 
 def start():
