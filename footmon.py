8e2742e05b9dcb2e7e8e4fb4b5cd64a4deb3dbda
antoniopelaezmoreno <antpelmor@alum.us.es>
2024-04-24 12:59:41 +0200

    funciones de poblar y eliminar base de datos

--- footmon.py
+++ footmon.py
@@ -2,10 +2,12 @@ from tkinter import Tk, Button
 import pandas as pd
 from pymongo import MongoClient
 
-def bd():
-    # Conexión a MongoDB
-    client = MongoClient('mongodb://localhost:27017/')
-    db = client['deportes_db']
+
+client = MongoClient('mongodb://localhost:27017/')
+db = client['deportes_db']
+
+def populate_db():
+    
     results = db['results']
     goalscorers = db['goalscorers']
     shootouts = db['shootouts']
@@ -25,8 +27,14 @@ def bd():
     goalscorers.insert_many(data_goalscorers_json)
     shootouts.insert_many(data_shootouts_json)
 
+    print("Datos insertados en la base de datos")
 
+def drop_db():
+    client.drop_database('deportes_db')
+
+    print("Base de datos eliminada")
     
+def partidos_equipo_año_torneo():
     # Función de map
     map_function = """
     function() {
@@ -48,11 +56,12 @@ def bd():
 
     sorted_result = list(db.partidos_por_equipo_por_año_en_torneo.find().sort([('year', -1)]))
 
-    print("Resultados de partidos por equipo por año en torneo: ")
+    print("\nResultados de partidos por equipo por año en torneo: ")
     for doc in sorted_result:
         print(doc)
-    
-    '''
+
+
+def goles_en_propia_reibidos_por_equipo():
     # Función de map
     map_function = """
     function() {
@@ -73,30 +82,26 @@ def bd():
 
     sorted_result = list(db.goles_en_propia.find().sort([('value', -1)]))
 
-    print("Goles en propia meta por equipo: ")
+    print("\n Goles en propia meta por equipo: ")
     for doc in sorted_result:
         print(doc)
-    '''
+
 def start():
     top=Tk()
-    B = Button(top, text ="Almacenar Resultados")
+    B = Button(top, text ="Popular BD", command=populate_db)
     B.pack()
     
-    B2 = Button(top, text ="Listar Jornadas")
+    B2 = Button(top, text ="Eliminar BD", command=drop_db)
     B2.pack()
-    
-    B3 = Button(top, text ="Buscar Jornadas")
+
+    B3 = Button(top, text ="Partidos de cada equipo en un año en competicion", command=partidos_equipo_año_torneo)
     B3.pack()
-    
-    B4 = Button(top, text ="Estadisticas Jornadas")
+
+    B4 = Button(top, text ="Goles en propia recibidos por cada equipo", command=goles_en_propia_reibidos_por_equipo)
     B4.pack()
-    
-    B5 = Button(top, text ="Buscar Goles")
-    B5.pack()
-    
+       
     top.mainloop()
 
 
 if __name__ == '__main__':
-    bd()
     start()
